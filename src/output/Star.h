18f47ca912acbaafa79a09eb65a67f3bbea5abe5
Jm Casler <jm@casler.org>
2023-07-17 20:43:03 -0700

    Give the poofer a little more poof

--- src/output/Star.h
+++ src/output/Star.h
@@ -13,8 +13,8 @@ private:
     uint8_t disabledBrightness = 0;
     uint8_t disabledBrightnessFade = 1; // This should be powers of two (1, 2, 4, 8, 16, 32, 64, etc)
 
-    uint32_t pooferIntervalMin = 90;
-    uint32_t pooferIntervalMax = 90;
+    uint32_t pooferIntervalMin = 100;
+    uint32_t pooferIntervalMax = 100;
 
     struct boomerData
     {
