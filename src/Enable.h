773aa1bac2473bb0c70f39d875833840f0f7cfaf
Jm Casler <jm@casler.org>
2023-08-10 15:24:47 -0700

    speed up brightness fade

--- src/Enable.h
+++ src/Enable.h
@@ -17,7 +17,7 @@ class Enable {
     uint8_t disabledBrightness = 0;
 
     // This defines the speed of the fade
-    uint8_t disabledBrightnessFade = 2; // This should be powers of two (1, 2, 4, 8, 16, 32, 64, etc)
+    uint8_t disabledBrightnessFade = 16; // This should be powers of two (1, 2, 4, 8, 16, 32, 64, etc)
 
     public: 
         Enable();
