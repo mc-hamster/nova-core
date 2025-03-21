d5ef38f0920d8c322c8f8e847cf4011367dc3c8a
Jm Casler <jm@casler.org>
2025-03-20 21:56:02 -0700

    remove captive portal

--- nova core/src/Web.h
+++ nova core/src/Web.h
@@ -4,7 +4,6 @@
 #pragma once
 
 #include <Arduino.h>
-#include <DNSServer.h>
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include "ESPAsyncWebServer.h"
@@ -16,28 +15,11 @@ extern bool GAME_ENABLED;
 extern bool SEQUENCE_LOCAL_ECHO;
 
 extern AsyncWebServer *server;
-extern DNSServer dnsServer;
 
 // Forward declarations
 void webSetup(void);
 void webLoop(void);
 ControlColor getColorForName(const char* colorName);
 
-// Simple captive portal handler
-class CaptiveRequestHandler : public AsyncWebHandler 
-{
-public:
-    CaptiveRequestHandler() {}
-    virtual ~CaptiveRequestHandler() {}
-
-    bool canHandle(AsyncWebServerRequest *request)
-    {
-      // request->addInterestingHeader("ANY");
-      return true;
-    }
-    void handleRequest(AsyncWebServerRequest *request) override {
-        request->send(200, "text/html", "<!DOCTYPE html><html><head><title>NOVA</title></head><body><p>NOVA Web Interface</p></body></html>");
-    }
-};
 
 #endif
